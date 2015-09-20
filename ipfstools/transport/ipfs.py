906623cee8324f8ebb1fa4615d184e51a622fdaf
Andrew Stocker <amstocker@dons.usfca.edu>
2015-09-19 19:40:18 -0700

    ipfs async api setup

--- ipfstools/transport/ipfs.py
+++ ipfstools/transport/ipfs.py
@@ -1,1 +1,11 @@
+"""
+Asynchronous version of ipfsApi (all we really have to do is patch socket).
+"""
+from gevent.monkey import patch_all; patch_all(thread=False, select=False)
+import gevent
+
 from ipfsApi import Client
+
+
+def map(requests):
+    pass
